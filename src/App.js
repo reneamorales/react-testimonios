0f62b797addbbea3c8da11c645cf82c52850a42e
reneamorales <94595230+reneamorales@users.noreply.github.com>
2022-09-30 11:09:17 -0300

    new project upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Testimonio from './components/Testimonio';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Testimonio / >
+        <
+        /div>
+    );
+}
+
+
+export default App;
